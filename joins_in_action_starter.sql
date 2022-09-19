8dc7994c392f249be1b6136d23852b2d21f322b9
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-19 12:37:40 -0400

    All problems solved

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -12,29 +12,54 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
 
+SELECT * FROM movies LEFT JOIN users on movie_id = favorite_movie_id
+
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
 
+SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
 
+SELECT * 
+FROM movies 
+INNER JOIN users 
+ON movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
 
+SELECT rotten_tomatoes, lead_studio 
+FROM movies 
+INNER JOIN users 
+on movie_id = favorite_movie_id
+ORDER BY rotten_tomatoes DESC
+LIMIT 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
+
+SELECT AVG(rotten_tomatoes)
+FROM movies 
+INNER JOIN users 
+on movie_id = favorite_movie_id;
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
+
+SELECT film, COUNT(*) f 
+FROM movies
+GROUP BY film
+HAVING f > 1;
+
+
 
